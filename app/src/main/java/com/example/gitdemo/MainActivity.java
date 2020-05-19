f0bdce066cf9b7ec571f9cf589608ceddd0162aa
LGC <1621853164@qq.com>
2020-05-19 17:47:24 +0800

    添加：搭建菜单归零

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -42,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
