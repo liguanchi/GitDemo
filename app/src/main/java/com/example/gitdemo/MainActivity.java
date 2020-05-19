770f568765329f14203314a068f96a77776231d0
LGC <1621853164@qq.com>
2020-05-19 16:31:57 +0800

    已修复：屏幕旋转导致number数据丢失问题

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gitdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonAdd;
     private Button mButtonSub;
     private TextView mTextView;
+    private static final String KEY_SAVE = "key_save";
     private int number = 0;
 
     @Override
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         mButtonAdd = findViewById(R.id.button_add);
         mButtonSub = findViewById(R.id.button_sub);
         mTextView = findViewById(R.id.text_view);
+
+        if (savedInstanceState !=null){
+            number = savedInstanceState.getInt(KEY_SAVE);
+            mTextView.setText(String.valueOf(number));
+        }
 
         mButtonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
                 mTextView.setText(String.valueOf(--number));
             }
         });
+
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_SAVE,number);
+    }
+
 }
